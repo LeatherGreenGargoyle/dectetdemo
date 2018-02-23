e59958c10062e145183fc6824c02a79f792e7f2d
leathergreengargoyle <avtran23@gmail.com>
2018-02-22 22:44:11 -0800

    Initial commit. Add test for card uniqueness.

---
+++ app/src/test/java/com/example/andrewtran/dectetdemo/FondationalTests.java
@@ -0,0 +1,33 @@
+package com.example.andrewtran.dectetdemo;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+/**
+ * Created by andrewtran on 2/22/18.
+ */
+
+public class FondationalTests {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void deck_hasOnlyUniqueCards() {
+//        Deck deck = new Deck();
+//        List<Card> drawnCards = ArrayList();
+//        while (deck.size != 0) {
+//            Card newCard = deck.draw();
+//            if (drawnCards.contains(newCard)) {
+//                fail("Drew a previously drawn card");
+//            }
+//            drawnCards.add(newCard);
+//        }
+//        drawnCards.add(deck.drawCard());
+    }
+}
