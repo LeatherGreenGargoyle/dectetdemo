c214776c3554c3bde9355b3277d8a22a638399b0
leathergreengargoyle <avtran23@gmail.com>
2018-03-06 22:57:57 -0800

    Make Space model and Orientation typedef.

--- app/src/main/java/com/example/andrewtran/dectetdemo/Models/Board.java
+++ app/src/main/java/com/example/andrewtran/dectetdemo/Models/Board.java
@@ -1,9 +1,23 @@
 package com.example.andrewtran.dectetdemo.Models;
 
+import android.support.annotation.IntDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 /**
  * Created by andrewtran on 2/24/18.
  * Playing field for a particular game
  */
 
 public class Board {
+
+    @Retention(RetentionPolicy.SOURCE)
+    @IntDef({UPRIGHT, POINTRIGHT, UPSIDEDOWN, POINTLEFT})
+    public @interface Orientation{}
+
+    public static final int UPRIGHT = 0;
+    public static final int POINTRIGHT = 1;
+    public static final int UPSIDEDOWN = 2;
+    public static final int POINTLEFT = 3;
 }
---
+++ app/src/main/java/com/example/andrewtran/dectetdemo/Models/Space.java
@@ -0,0 +1,16 @@
+package com.example.andrewtran.dectetdemo.Models;
+
+import java.util.List;
+
+/**
+ * Created by andrewtran on 3/6/18.
+ * A Space on a Board
+ */
+
+public class Space {
+
+     boolean mIsEmpty;
+     List<Card> mStack;
+
+
+}
